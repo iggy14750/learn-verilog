19e32664ee1b133b1f9b3779ff3283dabff93920
Isaac Goss <isaacgoss14750@gmail.com>
2024-04-09 17:11:27 -0400

    Very first self-checking DFlop testbench

--- sim/dflop_tb.cpp
+++ sim/dflop_tb.cpp
@@ -7,6 +7,18 @@
 
 #define NUM_CYCLES 100
 
+int num_errors = 0;
+
+#define ASSERT(condition) \
+    if (!condition) { \
+        printf("Error at line %d; cycle %d\n", __LINE__, cycle); \
+        num_errors++; \
+    }
+
+int random_bit() {
+    return rand() & 1;
+}
+
 int main(int argc, char** argv) {
 
     srand (time(NULL));
@@ -18,14 +30,25 @@ int main(int argc, char** argv) {
     top->trace(m_trace, 5);
     m_trace->open("waveform.vcd");
 
+    int last = 0;
+
     for (int cycle = 0; cycle < NUM_CYCLES; cycle++) {
         top->clk ^= 1;
+        top->reset = 0;
+
+        // The following only on a rising edge
+        if (top->clk == 1) {
+            top->in_1 = random_bit();
+            ASSERT(top->out_1 == last);
+            last = top->in_1;
+        }
         top->eval();
         m_trace->dump(cycle);
     }
 
     m_trace->close();
     printf("DFLOP SIM DONE!\n");
+    printf("\tErrors: %d\n", num_errors);
 
     delete top;
     return 0;
